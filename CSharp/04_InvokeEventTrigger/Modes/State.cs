305cd8960c82289e0aef657d962873a87e12bc20
bphnr900 <40142616+bphnr900@users.noreply.github.com>
2020-02-12 22:13:36 +0900

    Create State.cs

---
+++ CSharp/04_InvokeEventTrigger/Modes/State.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPartsStructures.Models
+{
+    class State
+    {
+        public int AreaID { get; set; }
+        public string Area { get; set; }
+        public int PrefectureID { get; set; }
+        public string Prefecture { get; set; }
+    }
+}
