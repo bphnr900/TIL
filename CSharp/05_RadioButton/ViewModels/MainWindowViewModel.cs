98cd6e5898899c62781b3db5b3be97ed4ebf30b5
bphnr900 <40142616+bphnr900@users.noreply.github.com>
2020-03-08 22:37:02 +0900

    Add files via upload

---
+++ CSharp/05_RadioButton/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace RadioButtonSample01.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        private string title = "Sample";
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+        public DelegateCommand SelectedCommand { get; private set; }
+
+        private bool isOverseaChecked;
+        public bool IsOverseaChecked
+        {
+            get { return isOverseaChecked; }
+            set { SetProperty(ref isOverseaChecked, value); }
+        }
+        private bool isDomesticChecked;
+        public bool IsDomesticChecked
+        {
+            get { return isDomesticChecked; }
+            set { SetProperty(ref isDomesticChecked, value); }
+        }
+
+        private string text = "選択してね";
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            SelectedCommand = new DelegateCommand(OnItemSelected);
+        }
+
+        private void OnItemSelected()
+        {
+            if (IsOverseaChecked)
+            {
+                Text = "海外が選択されたよ！";
+            }
+            if (isDomesticChecked)
+            {
+                Text = "国内が選択されたよ！";
+            }
+
+        }
+    }
+}
